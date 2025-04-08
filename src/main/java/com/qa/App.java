40b7211383c49c99f736da2e5ff327a4aa59239f
Karla <karla@lbg.com>
2025-04-08 12:57:32 +0100

    add my names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Nat"));
+        System.out.println(sayHelloToSomeone("Scott"));
+        System.out.println(sayHelloToSomeone("Chris"));
+        System.out.println(sayHelloToSomeone("Nick"));
         System.out.println(sayGoodbye());
     }
 
